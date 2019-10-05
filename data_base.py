3974a37f5fb64c5028ccb4c347b591b7ffa42cd0
Matt Mollison <matt.mollison@gmail.com>
2019-10-05 15:26:02 -0700

    Create the haiku table

--- data_base.py
+++ data_base.py
@@ -2,11 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.pool import NullPool
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from data_tweets_haiku import Haiku
 
 
 def session_factory(DATABASE_URL: str):
     engine = create_engine(DATABASE_URL, poolclass=NullPool)
     Base = declarative_base()
-    Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine, tables=[Haiku.__table__])
     _SessionFactory = sessionmaker(bind=engine)
     return _SessionFactory()

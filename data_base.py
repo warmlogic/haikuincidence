8b63d77875183e86f82b575538b49893a7b2fdb1
Matt Mollison <matt.mollison@gmail.com>
2019-10-05 13:19:55 -0700

    DB params as arguments

--- data_base.py
+++ data_base.py
@@ -1,26 +1,13 @@
-import configparser
-
 from sqlalchemy import create_engine
 from sqlalchemy.pool import NullPool
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-config = configparser.ConfigParser()
-config.read('config.ini')
 
-DB_USER = config['database'].get('DB_USER', '')
-DB_PASSWORD = config['database'].get('DB_PASSWORD', '')
-DB_SERVER = config['database'].get('DB_SERVER', 'localhost')
-DB_PORT = config['database'].get('DB_PORT', '5432')
-
-DB_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_SERVER}:{DB_PORT}/{DB_USER}"
-
-engine = create_engine(DB_URL, poolclass=NullPool)
-_SessionFactory = sessionmaker(bind=engine)
-
-Base = declarative_base()
-
-
-def session_factory():
+def session_factory(DB_USER, DB_PASSWORD, DB_SERVER, DB_PORT):
+    DB_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_SERVER}:{DB_PORT}/{DB_USER}"
+    engine = create_engine(DB_URL, poolclass=NullPool)
+    Base = declarative_base()
     Base.metadata.create_all(engine)
+    _SessionFactory = sessionmaker(bind=engine)
     return _SessionFactory()

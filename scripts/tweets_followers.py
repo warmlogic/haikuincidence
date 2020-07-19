212b6a592c01b06398901829dbcafda3c969bc38
Matt Mollison <matt.mollison@gmail.com>
2020-07-18 17:47:48 -0700

    updates to script

--- scripts/tweets_followers.py
+++ scripts/tweets_followers.py
@@ -34,45 +34,32 @@ twitter = Twython(
 
 # https://twython.readthedocs.io/en/latest/api.html
 
-# get the screen names I have replied to
-# can only make 1500 requests in a 15-minute window (100 per minute)
-# https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-user_timeline
-sleep_seconds = 1
-poets_list = []
-mytweets = twitter.get_user_timeline(screen_name=MY_SCREEN_NAME, count=200, exclude_replies='false', include_rts='true')
-poets_list.extend([tweet['in_reply_to_screen_name'] for tweet in mytweets])
-max_id = mytweets[-1]['id_str']
-while True:
-    logger.info(f'Tweet max id: {max_id}')
-    mytweets = twitter.get_user_timeline(screen_name=MY_SCREEN_NAME, count=200, exclude_replies='false', include_rts='true', max_id=max_id)
-    max_id_next = mytweets[-1]['id_str']
-    if max_id_next == max_id:
-        break
-    else:
-        max_id = max_id_next
-    poets_list.extend([tweet['in_reply_to_screen_name'] for tweet in mytweets])
-    logger.info(f'Sleeping for {sleep_seconds} seconds')
-    sleep(sleep_seconds)
-
-poets_list = list(set([sn for sn in poets_list if sn]))
-
 # get the screen names I follow
 # can only make 15 requests in a 15-minute window
 # https://developer.twitter.com/en/docs/accounts-and-users/follow-search-get-users/api-reference/get-friends-list
 sleep_seconds = 65
 cursor_if = -1
-i_follow_list = []
+i_follow = []
+counter = 1
 while True:
-    logger.info(f'I follow cursor: {cursor_if}')
-    ifollow = twitter.get_friends_list(screen_name=MY_SCREEN_NAME, count=200, skip_status='true', cursor=cursor_if)
-    if len(ifollow['users']) == 0:
+    logger.info(f'Query {counter}, I follow cursor: {cursor_if}')
+    result = twitter.get_friends_list(screen_name=MY_SCREEN_NAME, count=200, skip_status='true', cursor=cursor_if)
+    if len(result['users']) == 0:
         break
-    cursor_if = ifollow['next_cursor']
-    i_follow_list.extend([user['screen_name'] for user in ifollow['users']])
+    else:
+        counter += 1
+    user_list = [user['screen_name'] for user in result['users']]
+    i_follow.extend(user_list)
+    cursor_if = result['next_cursor']
+    logger.info(f'Added {len(user_list)} users who I follow (total: {len(i_follow)})')
     # # find the screen names with notifications turned on
-    # i_follow_list.extend([user['screen_name'] for user in ifollow['users'] if user['notifications']])
+    # user_list_notif = [user['screen_name'] for user in ifollow['users'] if user['notifications']]
+    # logger.info(f'Found {len(user_list_notif)} users with notifications turned on who I follow')
+    # i_follow.extend(user_list_notif)
     logger.info(f'Sleeping for {sleep_seconds} seconds')
     sleep(sleep_seconds)
+
+i_follow = list(set([sn for sn in i_follow if sn]))
 
 
 # get the screen names that follow me
@@ -80,51 +67,89 @@ while True:
 # https://developer.twitter.com/en/docs/accounts-and-users/follow-search-get-users/api-reference/get-followers-list
 sleep_seconds = 65
 cursor_fm = -1
-follow_me_list = []
+follows_me = []
+counter = 1
 while True:
-    logger.info(f'Follow me cursor: {cursor_fm}')
-    followme = twitter.get_followers_list(screen_name=MY_SCREEN_NAME, count=200, skip_status='true', cursor=cursor_fm)
-    if len(followme['users']) == 0:
+    logger.info(f'Query {counter}, Follow me cursor: {cursor_fm}')
+    result = twitter.get_followers_list(screen_name=MY_SCREEN_NAME, count=200, skip_status='true', cursor=cursor_fm)
+    if len(result['users']) == 0:
         break
-    cursor_fm = followme['next_cursor']
-    follow_me_list.extend([user['screen_name'] for user in followme['users']])
+    else:
+        counter += 1
+    user_list = [user['screen_name'] for user in result['users']]
+    follows_me.extend(user_list)
+    cursor_fm = result['next_cursor']
+    logger.info(f'Added {len(user_list)} users who follow me (total: {len(follows_me)})')
     logger.info(f'Sleeping for {sleep_seconds} seconds')
     sleep(sleep_seconds)
 
+follows_me = list(set([sn for sn in follows_me if sn]))
 
-# unfollow poets who no longer follow me
+
+# unfollow people I follow who do not follow me, to make room for following more new poets
 # https://developer.twitter.com/en/docs/accounts-and-users/follow-search-get-users/api-reference/post-friendships-destroy
-unfollowed_list = []
+unfollowed = []
+does_not_follow_me = set(i_follow) - set(follows_me)
+to_unfollow = list(set([sn for sn in does_not_follow_me if (sn not in unfollowed)]))
 sleep_seconds = 0.1
-to_unfollow_list = [sn for sn in i_follow_list if sn and (sn in poets_list) and (sn not in follow_me_list) and (sn not in unfollowed_list)]
-for sn in to_unfollow_list:
+for sn in to_unfollow:
     if sn:
         try:
-            unfollowed = twitter.destroy_friendship(screen_name=sn)
-            unfollowed_list.append(sn)
-            logger.info(f'unfollowed {sn}')
+            result = twitter.destroy_friendship(screen_name=sn)
+            unfollowed.append(sn)
+            logger.info(f'unfollowed {len(unfollowed)} / {len(to_unfollow)}: {sn}')
         except TwythonError:
             logger.exception(f'exception for {sn}')
     # logger.info(f'Sleeping for {sleep_seconds} seconds')
     sleep(sleep_seconds)
 
 
-# # follow accounts I have replied to but not followed
-# # https://developer.twitter.com/en/docs/accounts-and-users/follow-search-get-users/api-reference/post-friendships-create
-# # update notification settings
-# # https://developer.twitter.com/en/docs/accounts-and-users/follow-search-get-users/api-reference/post-friendships-update
-# to_follow_list = [sn for sn in poets_list if sn and (sn not in i_follow_list)]
-# sleep_seconds = 0.5
-# for sn in i_follow_list:
-#     if sn:
-#         try:
-#             # follow='false' means don't turn on notifications
-#             followed = twitter.create_friendship(screen_name=sn, follow='false')
-#             logger.info(f'followed {sn}')
-#             # device='false' means turn off notifications
-#             # followed = twitter.update_friendship(screen_name=sn, device='false')
-#             # logger.info(f'updated {sn}')
-#         except TwythonError:
-#             logger.exception(f'exception for {sn}')
-#     # logger.info(f'Sleeping for {sleep_seconds} seconds')
-#     sleep(sleep_seconds)
+# get the screen names I have replied to
+# can only make 1500 requests in a 15-minute window (100 per minute)
+# limited to the most recent 3200 tweets
+# https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-user_timeline
+counter = 1
+poets = []
+mytweets = twitter.get_user_timeline(screen_name=MY_SCREEN_NAME, count=200, exclude_replies='false', include_rts='true')
+poets.extend([tweet['in_reply_to_screen_name'] for tweet in mytweets])
+max_id = mytweets[-1]['id_str']
+sleep_seconds = 1
+while True:
+    logger.info(f'Query {counter}, Tweet max id: {max_id}')
+    mytweets = twitter.get_user_timeline(screen_name=MY_SCREEN_NAME, count=200, exclude_replies='false', include_rts='true', max_id=max_id)
+    max_id_next = mytweets[-1]['id_str']
+    if max_id_next == max_id:
+        break
+    else:
+        max_id = max_id_next
+        counter += 1
+    user_list = [tweet['in_reply_to_screen_name'] for tweet in mytweets]
+    poets.extend(user_list)
+    logger.info(f'Added {len(user_list)} users who I have replied to (total: {len(poets)})')
+    logger.info(f'Sleeping for {sleep_seconds} seconds')
+    sleep(sleep_seconds)
+
+poets = list(set([sn for sn in poets if sn]))
+
+
+# follow accounts I have replied to but not followed
+# https://developer.twitter.com/en/docs/accounts-and-users/follow-search-get-users/api-reference/post-friendships-create
+# update notification settings
+# https://developer.twitter.com/en/docs/accounts-and-users/follow-search-get-users/api-reference/post-friendships-update
+followed = []
+to_follow = list(set([sn for sn in poets if sn and (sn not in i_follow) and (sn not in followed)]))
+sleep_seconds = 5
+for sn in to_follow:
+    if sn:
+        try:
+            # follow='false' means don't turn on notifications
+            result = twitter.create_friendship(screen_name=sn, follow='false')
+            followed.append(sn)
+            logger.info(f'followed {len(followed)} / {len(to_follow)}: {sn}')
+            # device='false' means turn off notifications
+            # result = twitter.update_friendship(screen_name=sn, device='false')
+            # logger.info(f'updated {len(followed)} / {len(to_follow)}: {sn}')
+        except TwythonError:
+            logger.exception(f'exception for {sn}')
+    logger.info(f'Sleeping for {sleep_seconds} seconds')
+    sleep(sleep_seconds)

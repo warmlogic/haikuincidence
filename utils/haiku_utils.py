73c6352525e7edc4919c0fa46b83602b2c6d35e3
Matt Mollison <matt.mollison@gmail.com>
2020-07-19 14:45:17 -0700

    don't try to make potential acronym singlular

--- utils/haiku_utils.py
+++ utils/haiku_utils.py
@@ -123,7 +123,7 @@ def count_syllables(
             subtoken_syl = syllable_dict[remove_repeat_last_letter(subtoken)]['syllables']
             source = 'Dict (remove repeat)'
             subsyllable_count += subtoken_syl
-        elif (subtoken.endswith('s') or subtoken.endswith('z')) and (subtoken[:-1] in syllable_dict):
+        elif (subtoken.endswith('s') or subtoken.endswith('z')) and (subtoken[:-1] in syllable_dict) and not text_might_contain_acronym(subtoken_orig):
             subtoken_syl = syllable_dict[subtoken[:-1]]['syllables']
             source = 'Dict (singular)'
             subsyllable_count += subtoken_syl
@@ -135,7 +135,7 @@ def count_syllables(
             subtoken_syl = max([len([y for y in x if y[-1].isdigit()]) for x in pronounce_dict[remove_repeat_last_letter(subtoken)]])
             source = 'CMU (remove repeat)'
             subsyllable_count += subtoken_syl
-        elif (subtoken.endswith('s') or subtoken.endswith('z')) and (subtoken[:-1] in pronounce_dict):
+        elif (subtoken.endswith('s') or subtoken.endswith('z')) and (subtoken[:-1] in pronounce_dict) and not text_might_contain_acronym(subtoken_orig):
             subtoken_syl = max([len([y for y in x if y[-1].isdigit()]) for x in pronounce_dict[subtoken[:-1]]])
             source = 'CMU (singular)'
             subsyllable_count += subtoken_syl
